7a8a4c12feab4bab5fe62c9a8495954254805a42
Gabriel T Marques <gabriel.marques@mesainc.com.br>
2020-05-14 03:23:32 -0300

    adjusted user model

--- core/models.py
+++ core/models.py
@@ -5,4 +5,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=255)
     username = models.CharField(max_length=255)
-    access_token = models.CharField(max_length=255)
+    email = models.CharField(max_length=100, null=False)
+    access_token = models.CharField(max_length=255, null=False)
+    
+    avatar_url = models.CharField()
