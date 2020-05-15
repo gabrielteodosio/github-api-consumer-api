bb3ee94c0877de58c715d5e3f05a1c930fa5191c
Gabriel T Marques <gabriel.marques@mesainc.com.br>
2020-05-15 05:52:12 -0300

    adjusted user model to have some nullable fields

--- core/models.py
+++ core/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=255)
-    username = models.CharField(max_length=100)
-    email = models.CharField(max_length=100, null=False)
+    name = models.CharField(max_length=255, null=True)
+    username = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
     access_token = models.CharField(max_length=255, null=False)
 
     avatar_url = models.TextField()
